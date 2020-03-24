62343f5e49277bd21a47c3a0a588ac1a057c47e5
TakiMoysha <mpwema782@gmail.com>
2020-03-24 17:41:49 +0300

    init comit

--- Neuron-Net.py
+++ Neuron-Net.py
@@ -12,23 +12,28 @@ num7 = list('10100101')  # должен вернуть 7
 num8 = list('11111111')  # должен вернуть 8
 num9 = list('11101111')  # должен вернуть 9
 
-def test(ner):
-    ner.about()
-    print("0", ner.work(num0))
-    print("1", ner.work(num1))
-    print("2", ner.work(num2))
-    print("3", ner.work(num3))
-    print("4", ner.work(num4))
-    print("5", ner.work(num5))
-    print("6", ner.work(num6))
-    print("7", ner.work(num7))
-    print("8", ner.work(num8))
-    print("9", ner.work(num9))
+def test(neuron):
+    neuron.about()
+    print("0", neuron.work(num0))
+    print("1", neuron.work(num1))
+    print("2", neuron.work(num2))
+    print("3", neuron.work(num3))
+    print("4", neuron.work(num4))
+    print("5", neuron.work(num5))
+    print("6", neuron.work(num6))
+    print("7", neuron.work(num7))
+    print("8", neuron.work(num8))
+    print("9", neuron.work(num9))
 
+# ф-ция выдает результат, что это за число
+def dendrit(neurons, number):
+    sum = 0*int(neurons[0].work(number)) + 1*int(neurons[1].work(number)) + 2*int(neurons[2].work(number)) + 3*int(neurons[3].work(number)) + 4*int(neurons[4].work(number)) + 5*int(neurons[5].work(number)) + 6*int(neurons[6].work(number)) + 7*int(neurons[7].work(number)) + 8*int(neurons[8].work(number)) + 9*int(neurons[9].work(number))
+    print(sum)
 
+#  Обучение нейронов
 neurons = [neuron.Neuron() for i in range(10)]
 for i in range(len(neurons)):
     neurons[i].lerning(1000, i)
-    test(neurons[i])
+    # test(neurons[i])
 
-
+dendrit(neurons, num6)
